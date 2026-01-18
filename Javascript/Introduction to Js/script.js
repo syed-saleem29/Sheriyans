a78e9cd26d85db0b7030760561421ebad8bfd529
Syed Saleem <syedsaleem.2993@gmail.com>
2026-01-18 21:34:07 +0530

    Basics of javascript

---
+++ Javascript/Introduction to Js/script.js
@@ -0,0 +1,25 @@
+// alert();
+
+let a = 4;
+var b = 9;
+const c =5; // once set cannot be changed
+
+console.warn("this is warning");
+console.error("this is error");
+console.log("this is log");
+console.table({name: "saleem"})
+
+
+let str = "this is syed saleem, this is goint to be an exciting journey"
+
+console.log (str.split('a'));
+console.log (str.split('a').join(''));
+
+console.log (str.replace('a', "lululu"));
+console.log (str.replaceAll('a', "lululu"));
+
+console.log(str.includes('a'))
+console.log(`this is the sum of two num ${2+2}`)
+
+
+// prompt();
