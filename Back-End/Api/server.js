9d48b64cb7c9e2f2de829c5a5f7799a2953554c3
Syed Saleem <syedsaleem.2993@gmail.com>
2026-02-27 13:47:51 +0530

    Api

---
+++ Back-End/Api/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+
+app.post("/notes",(req, res)=>{
+    notes.push(req.body)
+    res.send("Note Created")
+    
+})
+
+app.get("/notes",(req, res)=>
+    res.send(notes)
+    
+)
+
+app.listen(3000,()=>{
+    console.log("Server is running at port 3000")
+})
