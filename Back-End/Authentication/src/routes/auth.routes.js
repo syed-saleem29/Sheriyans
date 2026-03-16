af141907143fdf09251d83b143e403c3d9f86f70
Syed Saleem <syedsaleem.2993@gmail.com>
2026-03-16 17:39:45 +0530

    Authentication - II (Hashing,Login)

--- Back-End/Authentication/src/routes/auth.routes.js
+++ Back-End/Authentication/src/routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
+const crypto = require('crypto')
 
 authRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -9,32 +10,76 @@ authRouter.post("/register", async (req, res) => {
   const isUserAlreadyExists = await userModel.findOne({ email });
 
   if (isUserAlreadyExists) {
-    return res.status(400).json({
+    return res.status(409).json({
       message: "User Already Exists with " + email,
     });
   }
 
+  const hash = crypto.createHash("md5").update(password).digest("hex")
+
   const user = await userModel.create({
     name,
     email,
-    password,
+    password:hash,
   });
 
   const token = jwt.sign(
     {
       id: user._id,
-      email: user.email
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(201).json({
+    message: "user registered",
+    user,
+    token,
+  });
+});
+
+authRouter.post("/protected", (req, res) => {
+  console.log(req.cookies);
+
+  res.status(200).json({
+    message: "Token Recieved",
+  });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user)
+    return res
+      .status(404)
+      .json({ message: "User not found with this email " + email });
+
+  const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex")
+;
+
+  if (!isPasswordMatched) {
+    return res.status(401).json({
+      message: "Invalid Password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
     },
     process.env.JWT_SECRET,
   );
 
   res.cookie("jwt_token",token)
 
-  res.status(201).json({
-    message: "user registered",
-    user,
-    token
-  });
+  res.status(200).json({
+    message:"User Logged In",
+    user
+  })
 });
 
 module.exports = authRouter;
